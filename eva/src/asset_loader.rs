e7016f1f9a4e0873b067d97123ccf067395ca22a
Devin Leamy <devinleamy@gmail.com>
2023-11-28 12:11:35 -0500

    Load assets by relative (rather than absolute) path

--- eva/src/asset_loader.rs
+++ eva/src/asset_loader.rs
@@ -7,24 +7,21 @@ pub struct AssetLoader;
 
 impl AssetLoader {
     pub fn load_texture_image(extension: &PathBuf) -> DynamicImage {
-        let mut path =
-            PathBuf::from("/Users/Devin/Desktop/Github/DevinLeamy/eva/eva/assets/textures/");
+        let mut path = PathBuf::from("./eva/assets/textures/");
         path.push(extension);
 
         Self::load_image(path)
     }
 
     pub fn load_skybox_image(extension: &PathBuf) -> DynamicImage {
-        let mut path =
-            PathBuf::from("/Users/Devin/Desktop/Github/DevinLeamy/eva/eva/assets/skybox/");
+        let mut path = PathBuf::from("./eva/assets/skybox/");
         path.push(extension);
 
         Self::load_image(path)
     }
 
     pub fn load_obj(extension: &PathBuf) -> ObjData {
-    let mut path =
-            PathBuf::from("/Users/Devin/Desktop/Github/DevinLeamy/eva/eva-py/assets/meshes/");
+        let mut path = PathBuf::from("./eva-py/assets/meshes/");
         path.push(extension);
 
         Obj::load(path).unwrap().data
